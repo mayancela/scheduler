df6f84ebcaed4ba10db8913b9a955168ad0fa1e0
anahi <anahimayancela@gmail.com>
2020-11-14 23:09:16 -0500

    adding schedule for each day of the week

---
+++ scheduler_handler.php
@@ -0,0 +1,44 @@
+<?php
+    $username = $_POST["username"]; // "name" value given to input
+    $day = $_POST["day"];
+
+    if($username != ""){
+        $strHeading = "<h3> Hi " . $_POST["username"] . "</h3>";
+    }
+    else{
+        $strHeading = "<h3> Hello there! </h3>";
+    }
+
+    echo $strHeading;
+
+    if ($day == "monday" || $day == "Monday") {
+        $events = "<br> Computer Architecture II Lecture <br> Web Design Recitation <br> Seminar Presentation";
+    }
+    elseif ($day == "tuesday" || $day == "Tuesday") {
+        $events = "<br> Calculus III Lecture <br> Intro to Physics <br> Soccer Practice";
+    }
+    elseif ($day == "wednesday" || $day == "Wednesday") {
+        $events = "<br> Computer II Lecture <br> Matrix Algebra <br> Dinner with Parents";
+    }
+    elseif ($day == "thursday" || $day == "Thursday") {
+        $events = "<br> Web Design Lecture <br> Lunch with Bella <br> Soccer Practice";
+    }
+    elseif ($day == "friday" || $day == "Friday") {
+        $events = "<br> Soccer Match <br> Matrix Algebra Project Due";
+    }
+    elseif ($day == "saturday" || $day == "Saturday") {
+        $events = "<br> Calculus III Homework Due <br> Bowling with Friends";
+    }
+    elseif ($day == "sunday" || $day == "Sunday") {
+        $events = "<br> Relaxation Day!";
+    }
+    else {
+        echo $day;
+        echo "Input a valid date";
+    }
+
+    if( $day != ""){
+        echo $day . "'s Schedule:" . $events;
+    }
+
+?>
