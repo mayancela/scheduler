2a809b8b85373c0c301803d194a3ac74a560ac55
anahi <anahimayancela@gmail.com>
2020-11-16 15:50:24 -0500

    adding boolean to check for edge case

--- scheduler_handler.php
+++ scheduler_handler.php
@@ -19,34 +19,41 @@
 
     echo $strHeading;
     $day_check = strtolower($day);
+    $check = false;
 
     if ($day_check == "monday") {
         $events = "<br> Computer Architecture II Lecture <br> Web Design Recitation <br> Seminar Presentation";
+        $check = true;
     }
     elseif ($day_check == "tuesday") {
         $events = "<br> Calculus III Lecture <br> Intro to Physics <br> Soccer Practice";
+        $check = true;
     }
     elseif ($day_check == "wednesday") {
         $events = "<br> Computer II Lecture <br> Matrix Algebra Lecture <br> Dinner with Parents";
+        $check = true;
     }
     elseif ($day_check == "thursday") {
         $events = "<br> Web Design Lecture <br> Lunch with Bella <br> Soccer Practice";
+        $check = true;
     }
     elseif ($day_check == "friday") {
         $events = "<br> Intro to Physics Lecture <br> Soccer Match <br> Matrix Algebra Project Due";
+        $check = true;
     }
     elseif ($day_check == "saturday") {
         $events = "<br> Calculus III Homework Due <br> Bowling with Friends";
+        $check = true;
     }
     elseif ($day_check == "sunday") {
         $events = "<br> Relaxation Day!";
+        $check = true;
     }
     else {
-        echo $day;
         echo "Input a valid date";
     }
 
-    if( $day != ""){
+    if( $day != "" && $check == true){
         $day = ucfirst($day_check);
         echo "<strong>" . $day . "'s Schedule: </strong>" . $events;
     }
