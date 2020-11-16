86eb09e8788c951a1290e30bb8e0205353fd9d8c
anahi <anahimayancela@gmail.com>
2020-11-16 15:37:23 -0500

    minor refactor and edge case considerations implemented

--- scheduler_handler.php
+++ scheduler_handler.php
@@ -11,33 +11,34 @@
     }
 
     if($username != ""){
-        $strHeading = "<h3> Hi " . $_POST['username'] . "</h3>";
+        $strHeading = "<h3> Hi " . ucfirst(strtolower($_POST['username'])) . "</h3>";
     }
     else{
         $strHeading = "<h3> Hello there! </h3>";
     }
 
     echo $strHeading;
+    $day_check = strtolower($day);
 
-    if ($day == "monday" || $day == "Monday") {
+    if ($day_check == "monday") {
         $events = "<br> Computer Architecture II Lecture <br> Web Design Recitation <br> Seminar Presentation";
     }
-    elseif ($day == "tuesday" || $day == "Tuesday") {
+    elseif ($day_check == "tuesday") {
         $events = "<br> Calculus III Lecture <br> Intro to Physics <br> Soccer Practice";
     }
-    elseif ($day == "wednesday" || $day == "Wednesday") {
-        $events = "<br> Computer II Lecture <br> Matrix Algebra <br> Dinner with Parents";
+    elseif ($day_check == "wednesday") {
+        $events = "<br> Computer II Lecture <br> Matrix Algebra Lecture <br> Dinner with Parents";
     }
-    elseif ($day == "thursday" || $day == "Thursday") {
+    elseif ($day_check == "thursday") {
         $events = "<br> Web Design Lecture <br> Lunch with Bella <br> Soccer Practice";
     }
-    elseif ($day == "friday" || $day == "Friday") {
-        $events = "<br> Soccer Match <br> Matrix Algebra Project Due";
+    elseif ($day_check == "friday") {
+        $events = "<br> Intro to Physics Lecture <br> Soccer Match <br> Matrix Algebra Project Due";
     }
-    elseif ($day == "saturday" || $day == "Saturday") {
+    elseif ($day_check == "saturday") {
         $events = "<br> Calculus III Homework Due <br> Bowling with Friends";
     }
-    elseif ($day == "sunday" || $day == "Sunday") {
+    elseif ($day_check == "sunday") {
         $events = "<br> Relaxation Day!";
     }
     else {
@@ -46,6 +47,7 @@
     }
 
     if( $day != ""){
+        $day = ucfirst($day_check);
         echo "<strong>" . $day . "'s Schedule: </strong>" . $events;
     }
 
